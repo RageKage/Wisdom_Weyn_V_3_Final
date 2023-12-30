da2a74e8e98cb672a1a095a345ba0f6df23e09b3
RageKage <99547837+RageKage@users.noreply.github.com>
2023-12-29 21:20:06 -0600

    updated voting logic but needs a little more work, created a new logo svg and added as well, turned the upvote up and votedowm into function because it doesn't the same thing but differeninily depending on if you up or down

--- client/src/components/nav/Logo.vue
+++ client/src/components/nav/Logo.vue
@@ -1,10 +1,25 @@
 <template>
-  <router-link to="/" class="text-3xl font-bold leading-none">
+  <router-link 
+    to="/"
+    class="text-3xl font-bold leading-none hover-img"
+    active-class="active-img"
+  >
     <img
       src="@/assets/images/Wisdom_weyn.svg"
-      class="h-24 "
-
+      class="h-12 md:h-16 lg:h-20 hover-img-scale hover-spin"
       alt="Wisdom Weyn"
     />
   </router-link>
 </template>
+
+<style>
+.hover-spin:hover {
+  transform: rotate(360deg);
+  transition: transform 0.6s ease-in-out;
+}
+
+.hover-spin {
+  transition: transform 0.6s ease-in-out;
+
+}
+</style>
