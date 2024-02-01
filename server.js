3a757b453e11108873cbe18fa96f1805871b4a91
RageKage <99547837+RageKage@users.noreply.github.com>
2024-02-01 00:24:32 -0600

    testing

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ let admin = require("firebase-admin");
 
 // initialize Firebase Admin using our env called ADMIN_SDK
 // console.log(process.env);
-var serviceAccount = require(process.env.ADMIN_SDK)
+var serviceAccount = require('./wisdom-weyn-firebase-adminsdk-cmkcb-2df3bce2fb.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
