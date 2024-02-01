8e1a297421d628866aeee564917d250994b9b2da
RageKage <99547837+RageKage@users.noreply.github.com>
2024-02-01 00:43:45 -0600

    render server is up and running

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ import 'dotenv/config'
 
 // get the link to our server from our .env
 // eslint-disable-next-line no-undef
-// const apiPath = process.env.VITE_NGROK_URL
+const apiPath = process.env.VITE_API_URL
 // const apiPath = 'https://0d80-2601-243-822-1e1a-98a0-a8bf-1d4b-3149.ngrok-free.app'
 
 export default defineConfig({
@@ -17,23 +17,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  // // ! use this when you wanna host it
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: apiPath,
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ''),
-  //     },
-  //   },
-  // },
-
-  // ! run locally
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
-    },
-  },
+      '/api': {
+        target: apiPath,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }},
 
   optimizeDeps: {
     include: ['@heroicons/vue'],
