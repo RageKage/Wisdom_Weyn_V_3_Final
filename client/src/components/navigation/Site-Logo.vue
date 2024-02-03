d199a59d4e031f69bc31a5f21752236ec16ee1cd
RageKage <99547837+RageKage@users.noreply.github.com>
2024-02-02 19:40:26 -0600

    Remove .env.production from tracking

--- client/src/components/navigation/Site-Logo.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <router-link to="/" active-class="active-img">
-    <img
-      src="../../assets/images/wisdom_weyn_logo_2.svg"
-      alt="logo"
-      class="flex items-center justify-center space-x-3 sm:pr-4 md:pr-4 lg:pr-0 w-16 h-16 md:w-16 md:h-16 lg:w-20 lg:h-20 transition-all duration-300 ease-in-out transform hover:transition-all duration 300 hover:rotate-12 hover:scale-110"
-    />
-  </router-link>
-</template>
